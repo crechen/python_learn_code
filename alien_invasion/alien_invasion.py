66e9fd5e12c173e58234df9d02c6b0da522aef00
crechen <chen45430@gmail.com>
2018-04-06 16:17:14 +0800

    python编程从入门到实践；外星人入侵游戏

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,25 @@
+import pygame
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+def run_game():
+	#初始化游戏并创建一个屏幕对象
+	pygame.init()
+	ai_settings = Settings()
+	screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+	pygame.display.set_caption("外星人入侵")
+	
+	# 创建一艘飞船
+	ship = Ship(screen)
+	
+	# 开始游戏的主循环
+	while True:
+		
+		# 监视键盘和鼠标事件
+		gf.check_events(ship)
+		ship.update()
+		gf.update_screen(ai_settings, screen, ship)
+		
+run_game()
